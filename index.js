e3cce976d45467ab342cc74467902792ee32cf66
Rashid-Faheem <mrashidfaheem@gmail.com>
2024-04-04 09:50:07 +0500

    Login

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-console.log("Helloworld!");
-export {};
+import inquirer from "inquirer";
+console.log(`---------------------------------------------------------`);
+console.log(`                   Welcome to My Bank                   `);
+console.log(`---------------------------------------------------------`);
+const userPin = 3771;
+let isLogin = false;
+let counter = 4;
+while (isLogin === false && counter > 0) {
+    let pinNumber = await inquirer.prompt([
+        {
+            message: "Enter Pin Number",
+            name: "pinEntered",
+            type: "number",
+        },
+    ]);
+    if (pinNumber.pinEntered === userPin) {
+        console.log(`---------------------------------------------------------`);
+        console.log(`                         Welcome                         `);
+        console.log(`---------------------------------------------------------`);
+        isLogin = true;
+    }
+    else {
+        console.log("Entered Pin is incorrect!");
+        isLogin = false;
+        counter--;
+    }
+}
--- index.ts
+++ index.ts
@@ -1,2 +1,28 @@
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
 
+console.log(`---------------------------------------------------------`);
+console.log(`                   Welcome to My Bank                   `);
+console.log(`---------------------------------------------------------`);
+
+const userPin: number = 3771;
+let isLogin: boolean = false;
+let counter:number = 4;
+while (isLogin === false && counter > 0) {
+let pinNumber = await inquirer.prompt([
+  {
+    message: "Enter Pin Number",
+    name: "pinEntered",
+    type: "number",
+  },
+]);
+if (pinNumber.pinEntered === userPin) {
+    console.log(`---------------------------------------------------------`);
+    console.log(`                         Welcome                         `);
+    console.log(`---------------------------------------------------------`);
+  isLogin= true;
+} else {
+  console.log("Entered Pin is incorrect!");
+  isLogin= false;
+  counter--;
+}
+}
