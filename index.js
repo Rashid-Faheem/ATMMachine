3e89d4022155b9f2a9ac8cf5d492549d4c326b34
Rashid-Faheem <mrashidfaheem@gmail.com>
2024-04-04 17:49:09 +0500

    v2

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ console.log(`                   Welcome to My Bank                   `);
 console.log(`---------------------------------------------------------`);
 const userPin = 3771;
 let isLogin = false;
+let isok = false;
 let counter = 4;
+let accBalance = 8500;
 while (isLogin === false && counter > 0) {
     let pinNumber = await inquirer.prompt([
         {
@@ -15,10 +17,43 @@ while (isLogin === false && counter > 0) {
         },
     ]);
     if (pinNumber.pinEntered === userPin) {
+        isLogin = true;
         console.log(`---------------------------------------------------------`);
         console.log(`                         Welcome                         `);
         console.log(`---------------------------------------------------------`);
-        isLogin = true;
+        let option1 = await inquirer.prompt([
+            {
+                message: "Select Option!",
+                name: "selectedOption1",
+                type: "list",
+                choices: ["Show Balance", "Cash Withdrawl", "Fast Cash", "Cancel"],
+            },
+        ]);
+        if (option1.selectedOption1 === "Show Balance") {
+            console.log(`your Account balance is Rs. ${accBalance}`);
+        }
+        else if (option1.selectedOption1 === "Cash Withdrawl") {
+            while (isok === false) {
+                let cashWD = await inquirer.prompt([
+                    { message: "Enter Amount:", type: "number", name: "cwdamt" },
+                ]);
+                let mod = cashWD.cwdamt % 500;
+                //console.log(mod);
+                if (mod === 0) {
+                    if (accBalance < cashWD.cwdamt) {
+                        console.log("Insufficient Balance!");
+                    }
+                    else {
+                        accBalance -= cashWD.cwdamt;
+                        console.log(`Take your Cash! Remaining Balance Rs. ${accBalance}`);
+                        isok = true;
+                    }
+                }
+                else {
+                    console.log("Amount must be rounded upto 500");
+                }
+            }
+        }
     }
     else {
         console.log("Entered Pin is incorrect!");
--- index.ts
+++ index.ts
@@ -7,23 +7,62 @@ console.log(`---------------------------------------------------------`);
 
 const userPin: number = 3771;
 let isLogin: boolean = false;
-let counter:number = 4;
+let isok: boolean = false;
+let counter: number = 4;
+let accBalance: number = 8500;
+
 while (isLogin === false && counter > 0) {
-let pinNumber = await inquirer.prompt([
-  {
-    message: "Enter Pin Number",
-    name: "pinEntered",
-    type: "number",
-  },
-]);
-if (pinNumber.pinEntered === userPin) {
+  let pinNumber = await inquirer.prompt([
+    {
+      message: "Enter Pin Number",
+      name: "pinEntered",
+      type: "number",
+    },
+  ]);
+  if (pinNumber.pinEntered === userPin) {
+    isLogin = true;
     console.log(`---------------------------------------------------------`);
     console.log(`                         Welcome                         `);
     console.log(`---------------------------------------------------------`);
-  isLogin= true;
-} else {
-  console.log("Entered Pin is incorrect!");
-  isLogin= false;
-  counter--;
-}
+    let option1 = await inquirer.prompt([
+      {
+        message: "Select Option!",
+        name: "selectedOption1",
+        type: "list",
+        choices: ["Show Balance", "Cash Withdrawl", "Fast Cash", "Cancel"],
+      },
+    ]);
+
+    if (option1.selectedOption1 === "Show Balance") {
+      console.log(`your Account balance is Rs. ${accBalance}`);
+    } else if (option1.selectedOption1 === "Cash Withdrawl") {
+      while (isok === false) {
+        let cashWD = await inquirer.prompt([
+          { message: "Enter Amount:", type: "number", name: "cwdamt" },
+        ]);
+
+        let mod = cashWD.cwdamt % 500;
+
+        //console.log(mod);
+        if (mod === 0) {
+         
+          if (accBalance < cashWD.cwdamt) {
+            console.log("Insufficient Balance!");
+          }
+          else{
+            accBalance -= cashWD.cwdamt;
+            console.log(`Take your Cash! Remaining Balance Rs. ${accBalance}`);
+            isok = true;
+          }
+        }
+        else{
+          console.log("Amount must be rounded upto 500");
+        }
+      }
+    }
+  } else {
+    console.log("Entered Pin is incorrect!");
+    isLogin = false;
+    counter--;
+  }
 }
